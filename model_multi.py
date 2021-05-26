9dd95ed014f8f99b057a3ff1cbbd1e622edc5441
aby03 <abhay.saxena003@gmail.com>
2021-05-26 23:51:05 +0530

    merging

---
+++ model_multi.py
@@ -0,0 +1,90 @@
+from model import *
+
+def build_EfficientGrasp_multi(phi,
+                        freeze_bn = False,
+                        print_architecture = False):
+    # Get Parameters for model
+    assert phi in range(7)
+    scaled_parameters = get_scaled_parameters(phi)
+    
+    input_size = scaled_parameters["input_size"]
+    input_shape = (input_size, input_size, 3)
+    backbone_class = scaled_parameters["backbone_class"]
+    bifpn_width = scaled_parameters["bifpn_width"]
+    subnet_width = scaled_parameters["subnet_width"]
+    bifpn_depth = scaled_parameters["bifpn_depth"]
+    subnet_depth = scaled_parameters["subnet_depth"]
+    subnet_num_iteration_steps = scaled_parameters["subnet_num_iteration_steps"]
+    num_groups_gn = scaled_parameters["num_groups_gn"]
+
+    output_dim = 6
+    pred_count = 100
+
+    # Input layer
+    image_input = layers.Input(input_shape)
+    
+    # 1. Build EfficientNet backbone
+    backbone_feature_maps = backbone_class(input_tensor = image_input)
+
+    # 2. Build BiFPN
+    fpn_feature_maps = build_BiFPN(backbone_feature_maps, bifpn_depth, bifpn_width, num_groups_gn, freeze_bn)
+
+    # 3. Build GraspNet
+    grasp_net = GraspNet(subnet_width,
+                        subnet_depth,
+                        num_iteration_steps = subnet_num_iteration_steps,
+                        freeze_bn=freeze_bn, 
+                        use_group_norm = True,
+                        num_groups_gn = num_groups_gn,
+                        name='grasp_net')
+    
+    # Get reshape dims for applying grasp net
+    reshape_dim = 0     # 64x64 + 32x32 + 16x16 + 8x8 + 4x4
+    for i in range(len(fpn_feature_maps)):
+        reshape_dim += fpn_feature_maps[i].shape[1]*fpn_feature_maps[i].shape[2]
+    
+    # Apply GraspNet
+    grasp_regression = [grasp_net([feature, i]) for i, feature in enumerate(fpn_feature_maps)]
+    grasp_regression = layers.Concatenate(axis=1, name='regression_con')(grasp_regression)
+    grasp_regression = layers.Reshape((-1,reshape_dim * output_dim))(grasp_regression) # 5456 for num_anchors=1 && 49104 for 9 
+
+    grasp_regression_multi = layers.Dense(pred_count * output_dim, name='regression_grasp')(grasp_regression)
+    grasp_regression_multi = layers.Reshape((pred_count, output_dim), name='regression_grasp_re')(grasp_regression_multi)
+    grasp_regression_score = layers.Dense(pred_count, name='regression_score', activation=tf.keras.activations.sigmoid)(grasp_regression)
+    grasp_regression_score = layers.Reshape((pred_count,1), name='regression_score_re')(grasp_regression_score)
+
+    grasp_regression_out = layers.Concatenate(axis=2, name='regression_out')([grasp_regression_multi, grasp_regression_score])
+
+    # grasp_regression = layers.Dense(output_dim, name='regression_out')(grasp_regression)
+    # # Reshape to (batch, 1, 6) and then Concatenate 30 times
+    # grasp_regression_train = layers.Concatenate(axis=1, name="final_layer")([grasp_regression for x in range(0, 30)])
+
+    # # Prediction is (batch, 6)
+    # grasp_regression_pred = layers.Flatten(name='regression_p')(grasp_regression)
+
+    # Build Complete Model
+    efficientgrasp_train = models.Model(inputs = [image_input], outputs = grasp_regression_out, name = 'efficientgrasp')
+    efficientgrasp_prediction = models.Model(inputs = [image_input], outputs = grasp_regression_out, name = 'efficientgrasp_prediction')
+
+
+    # # Debug Architecture
+    # grasp_regression = layers.MaxPooling2D(pool_size=(10, 10), strides=(10, 10), padding='valid')(image_input)
+    # grasp_regression = layers.Flatten()(grasp_regression)
+
+    # # Final Layer into 6 dim vector
+    # grasp_regression = layers.Dense(output_dim, name='regression')(grasp_regression)
+
+    # # Reshape to (batch, 1, 6) and then Concatenate 30 times
+    # grasp_regression = layers.Reshape(target_shape=(1,6))(grasp_regression)
+    # grasp_regression = layers.Concatenate(axis=1, name="final_layer")([grasp_regression for x in range(0, 30)])
+
+    # efficientgrasp_train = models.Model(inputs = [image_input], outputs = grasp_regression, name = 'efficientgrasp')
+    
+    all_layers = list(set(efficientgrasp_train.layers + efficientgrasp_prediction.layers))
+
+    if print_architecture:
+        efficientgrasp_train.summary()
+    # Return Model
+    return efficientgrasp_train, efficientgrasp_prediction, all_layers
+
+# build_EfficientGrasp_multi(0, print_architecture=True)
---
+++ train_multi.py
@@ -0,0 +1,276 @@
+import argparse
+import sys
+import time
+import os
+import json
+# Preprocessing
+from dataset_processing.cornell_generator import CornellDataset
+
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Optimization after profiling
+os.environ['TF_GPU_THREAD_MODE'] = 'gpu_private'
+
+# Model related
+from model_multi import build_EfficientGrasp_multi
+from tensorflow import keras
+from tensorflow.keras.optimizers import Adam
+from losses import grasp_loss_multi
+from custom_load_weights import custom_load_weights
+from efficientnet import BASE_WEIGHTS_PATH, WEIGHTS_HASHES
+
+def parse_args(args):
+    """
+    Parse the arguments.
+    """
+    date_and_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+    parser = argparse.ArgumentParser(description = 'EfficientGrasp training script.')
+
+    subparsers = parser.add_subparsers(help = 'Arguments for specific dataset types.', dest = 'dataset_type')
+    subparsers.required = True
+    cornell_parser = subparsers.add_parser('cornell')
+    cornell_parser.add_argument('cornell_path', help = 'Path to dataset directory (ie. Datasets/Cornell/archive/).')
+
+    parser.add_argument('--weights', help = 'File containing weights to init the model parameter')
+    parser.add_argument('--freeze-backbone', help = 'Freeze training of backbone layers.', action = 'store_true')
+
+    parser.add_argument('--batch-size', help = 'Size of the batches.', default = 1, type = int)
+    parser.add_argument('--lr', help = 'Learning rate', default = 1e-4, type = float)
+    parser.add_argument('--phi', help = 'Hyper parameter phi', default = 0, type = int, choices = (0, 1, 2, 3, 4, 5, 6))
+    parser.add_argument('--gpu', help = 'Id of the GPU to use (as reported by nvidia-smi).')
+    parser.add_argument('--epochs', help = 'Number of epochs to train.', type = int, default = 100)
+    parser.add_argument('--start-epoch', help = 'Epoch count to start for resuming training', dest = 'start_epoch', type = int, default = 0)
+    parser.add_argument('--steps', help = 'Number of steps per epoch.', type = int, default = int(179 * 10))
+    parser.add_argument('--snapshot-path', help = 'Path to store snapshots of models during training', default = os.path.join("checkpoints", date_and_time))
+    parser.add_argument('--tensorboard-dir', help = 'Log directory for Tensorboard output', default = os.path.join("logs", date_and_time))
+    parser.add_argument('--no-snapshots', help = 'Disable saving snapshots.', dest = 'snapshots', action = 'store_false')
+    parser.add_argument('--no-evaluation', help = 'Disable per epoch evaluation.', dest = 'evaluation', action = 'store_false')
+
+    # Fit generator arguments
+    parser.add_argument('--multiprocessing', help = 'Use multiprocessing in fit_generator.', action = 'store_true')
+    parser.add_argument('--workers', help = 'Number of generator workers.', type = int, default = 1)
+    parser.add_argument('--max-queue-size', help = 'Queue length for multiprocessing workers in fit_generator.', type = int, default = 1)
+    
+    print(vars(parser.parse_args(args)))
+    return parser.parse_args(args)
+
+def main(args = None):
+    """
+    Train an EfficientGrasp model.
+
+    Args:
+        args: parseargs object containing configuration for the training procedure.
+    """
+    # Fix cudnn initialization error for tf 1.15 py3.7 CUDA 10.2 EfficientGrasp environment 
+    # import tensorflow as tf
+    # physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    # assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+    # config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    # import tensorflow as tf
+    # physical_devices = tf.config.list_physical_devices('GPU') 
+    # tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    # parse arguments
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+
+    # create the generators
+    print("\nCreating the Generators...\n")
+    train_generator, validation_generator = create_generators(args)
+    print("Done!")
+
+    # optionally choose specific GPU
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    print("\nBuilding Model!\n")
+    model, prediction_model, all_layers = build_EfficientGrasp_multi(args.phi,
+                                print_architecture=False)
+
+    # load pretrained weights
+    if args.weights:
+        if args.weights == 'imagenet':
+            model_name = 'efficientnet-b{}'.format(args.phi)
+            file_name = '{}_weights_tf_dim_ordering_tf_kernels_autoaugment_notop.h5'.format(model_name)
+            file_hash = WEIGHTS_HASHES[model_name][1]
+            weights_path = keras.utils.get_file(file_name,
+                                                BASE_WEIGHTS_PATH + file_name,
+                                                cache_subdir='models',
+                                                file_hash=file_hash)
+            model.load_weights(weights_path, by_name=True)
+        else:
+            print('Loading model, this may take a second...')
+            custom_load_weights(filepath = args.weights, layers = all_layers, skip_mismatch = True)
+            print("\nCustom Weights Loaded!\n")
+
+    # freeze backbone layers
+    if args.freeze_backbone:
+        # 227, 329, 329, 374, 464, 566, 656
+        for i in range(1, [227, 329, 329, 374, 464, 566, 656][args.phi]):
+            model.layers[i].trainable = False
+
+    print("\nCompiling Model!\n")
+    model.compile(  
+                    optimizer=Adam(lr = args.lr, clipnorm = 0.001),
+                    loss={'regression_out': grasp_loss_multi(args.batch_size),
+                        #   'regression_score' score_loss_multi(),
+                         }
+                    # metric=['grasp_accuracy']
+                 )
+
+    # create the callbacks
+    callbacks = create_callbacks(
+        model,
+        prediction_model,
+        validation_generator,
+        args,
+    )
+    
+    print("\nStarting Training!\n")
+    history = model.fit_generator(
+        generator = train_generator,
+        # steps_per_epoch = 2,
+        steps_per_epoch = train_generator.batches_per_epoch(),
+        initial_epoch = args.start_epoch,
+        epochs = args.epochs,
+        # epochs = 1,
+        verbose = 1,
+        callbacks = callbacks,
+        workers = args.workers,
+        use_multiprocessing = args.multiprocessing,
+        max_queue_size = args.max_queue_size,
+        validation_data = validation_generator
+    )
+    print("\nTraining Complete! Saving...\n")
+    os.makedirs(args.snapshot_path, exist_ok = True)
+    # NOT WORKING
+    model.save(os.path.join(args.snapshot_path, '{dataset_type}_finish.h5'.format(dataset_type = args.dataset_type)))
+    print("\nEnd of Code...\n")
+    
+
+
+def create_generators(args):
+    """
+    Create generators for training and validation.
+    Args:
+        args: parseargs object containing configuration for generators.
+    Returns:
+        The training and validation generators.
+    """
+    common_args = {
+        'batch_size': args.batch_size,
+        'phi': args.phi,
+    }
+    
+    if args.dataset_type == 'cornell':
+        dataset = args.cornell_path
+        # open output file for reading
+        with open(dataset+'/train_1.txt', 'r') as filehandle:
+            train_data = json.load(filehandle)
+        with open(dataset+'/valid_1.txt', 'r') as filehandle:
+            valid_data = json.load(filehandle)
+        
+        train_generator = CornellDataset(
+            dataset,
+            train_data,
+            **common_args
+        )
+
+        validation_generator = CornellDataset(
+            dataset,
+            valid_data,
+            train=False,
+            **common_args
+        )
+    else:
+        raise ValueError('Invalid data type received: {}'.format(args.dataset_type))
+
+    return train_generator, validation_generator
+
+def create_callbacks(training_model, prediction_model, validation_generator, args):
+    """
+    Creates the callbacks to use during training.
+
+    Args:
+        training_model: The model that is used for training.
+        prediction_model: The model that should be used for validation.
+        validation_generator: The generator for creating validation data.
+        args: parseargs args object.
+
+    Returns:
+        A list of callbacks used for training.
+    """
+    callbacks = []
+
+    tensorboard_callback = None
+    
+    if args.dataset_type == "cornell":
+        snapshot_path = args.snapshot_path
+        # save_path = args.validation_image_save_path
+        tensorboard_dir = args.tensorboard_dir
+        metric_to_monitor = "grasp_accuracy"
+        mode = "max"
+    else:
+        snapshot_path = args.snapshot_path
+        # save_path = args.validation_image_save_path
+        tensorboard_dir = args.tensorboard_dir
+        
+    # if save_path:
+    #     os.makedirs(save_path, exist_ok = True)
+
+    if tensorboard_dir:
+        tensorboard_callback = keras.callbacks.TensorBoard(
+            log_dir = tensorboard_dir,
+            histogram_freq = 1,
+            write_graph = True,
+            write_grads = False,
+            write_images = False,
+            embeddings_freq = 0,
+            embeddings_layer_names = None,
+            embeddings_metadata = None,
+            profile_batch = '25,30'
+        )
+        callbacks.append(tensorboard_callback)
+
+    if args.evaluation and validation_generator:
+        from eval.eval_callback import Evaluate
+        evaluation = Evaluate(validation_generator, prediction_model, tensorboard = tensorboard_callback, multi_pred=True)
+        evaluation._supports_tf_logs = True
+        callbacks.append(evaluation)
+
+    # Learning Rate Schedule
+    callbacks.append(keras.callbacks.ReduceLROnPlateau(
+        monitor    = 'loss',
+        factor     = 0.5,
+        patience   = 10,
+        verbose    = 1,
+        mode       = 'min',
+        min_delta  = 0.0001,
+        cooldown   = 0,
+        min_lr     = 1e-7
+    ))
+    
+    # save the model
+    if args.snapshots:
+        # ensure directory created first; otherwise h5py will error after epoch.
+        os.makedirs(snapshot_path, exist_ok = True)
+        # checkpoint = keras.callbacks.ModelCheckpoint(os.path.join(snapshot_path, 'phi_{phi}_{dataset_type}_best_{metric}.h5'.format(phi = str(args.phi), metric = metric_to_monitor, dataset_type = args.dataset_type)),
+        checkpoint = keras.callbacks.ModelCheckpoint(os.path.join(snapshot_path, '{dataset_type}_best_{metric}.h5'.format(phi = str(args.phi), metric = metric_to_monitor, dataset_type = args.dataset_type)),
+                                                     verbose = 1,
+                                                     save_weights_only = True,
+                                                    #  save_best_only = True,
+                                                     monitor = metric_to_monitor,
+                                                     save_freq='epoch',
+                                                     mode = mode)
+        # checkpoint._supports_tf_logs = False
+        callbacks.append(checkpoint)
+
+    return callbacks
+
+if __name__ == '__main__':
+    main()
